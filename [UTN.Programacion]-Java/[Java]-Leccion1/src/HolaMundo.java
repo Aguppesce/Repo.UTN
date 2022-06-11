2ec1ecd02d28efc7184278d3a86f24b4d7d7e856
Aguppesce <aguppesce@gmail.com>
2022-06-11 16:05:09 -0300

    Java Clase 3 22/04/2022

--- [UTN.Programacion]-Java/[Java]-Leccion1/src/HolaMundo.java
+++ [UTN.Programacion]-Java/[Java]-Leccion1/src/HolaMundo.java
@@ -24,6 +24,41 @@ public class HolaMundo {
                 miVariableCadena = "Sigamos creciendo en programación";
                 System.out.println(miVariableCadena);
 				*/				
-		
+				
+				//=====================================CLASE 3 - VIERNES-22/04/2022===================================
+				//Var - inferencia de tipos en Java
+                /*var miVariableEntera2 = 10;
+                var miVariableCadena2 = "Seguimos estudiando";
+                System.out.println("miVariableEntera2 = "+miVariableEntera2);
+                System.out.println("miVariableCadena2 = "+miVariableCadena2);
+                //soutv + tab
+                //Para ejecutar Shift + F6 es la tecla máyuscula
+                //Reglas para definir una variable en Java
+
+                var usuario="Osvaldo";
+                var titulo="Ingeniero";
+                var union=titulo + " " + usuario;
+                System.out.println("union = " + union);
+
+                var a = 8;
+                var b = 4;
+                System.out.println(usuario + (a+b));
+                //Ejercicio: Caracteres Especiales con Java
+                var nombre = "Natalia";
+                System.out.println("\nNuevo linea: \n"+nombre);//Diagonal inversa y letra n
+                System.out.println("\t\t.:MENU:.");
+                System.out.println("Retroseso: \b\b"+nombre);//Caracteres de Retroseso
+                System.out.println("Comillas simples: \'"+nombre+"\'");
+                System.out.println("Comillas Dobles: \""+nombre+"\"");*/
+
+                //Clase Scanner
+                /*Scanner entrada = new Scanner(System.in);
+                System.out.println("Digite su nombre: ");
+                var usuario2 = entrada.nextLine();
+                System.out.println("usuario2 = " + usuario2);
+                System.out.println("Escriba el titulo: ");
+                var titulo2 = entrada.nextLine();
+                System.out.println("Resultado: "+titulo2+" "usuario2);*/
+				
 		}
 }
