4fc2728d1875b76fbde94f52842d662723a12a18
Aguppesce <aguppesce@gmail.com>
2022-06-11 14:01:13 -0300

    Python Clase 2 13/04/2022

---
+++ [UTN.Laboratorio]-Python/[Python]-Leccion1/main.py
@@ -0,0 +1,26 @@
+# =====================================MIERCOLES-06/04/2022====================================='
+
+"""print("Hola Mundo")"""
+
+# =====================================MIERCOLES-13/04/2022====================================='
+
+'''miVariable = 3
+print(miVariable)
+
+miVariable = "Hola a todos los alumnos de la tecnicatura"
+print(miVariable)
+
+miVariable = 3.5
+print(miVariable)
+
+x = 10
+y = 2 # Literal de tipo numérico
+z = x + y
+# Las literales se escriben x240, la variable y=x984, la variable z=x304
+
+print(id(x)) # Para ver la dirección de memoria
+print(id(y))
+print(id(z))
+
+a = False # Esta es una variable literal de tipo booleana
+print(type(a))'''
