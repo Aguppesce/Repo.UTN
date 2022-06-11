a1c6b6acabdc6c57e6cbc87a0dcc4ce606e10e4b
Aguppesce <aguppesce@gmail.com>
2022-06-11 16:34:08 -0300

    Python Clase 6 11/05/2022

---
+++ [UTN.Laboratorio]-Python/[Python]-Leccion2/Ejercicio1.py
@@ -0,0 +1,46 @@
+#===================================CLASE 6 - MIERCOLES-11/05/2022===================================
+
+import math
+
+'''
+# Ejercicio 1:
+# Deberemos plasmar la expresión en una expresión
+# algoritmica, osea hacerlo en código
+
+a = float(input('Digite el valor de a: '))
+b = float(input('Digite el valor de b: '))
+c = float(input('Digite el valor de c: '))
+resultado = (a**3 * (b**2-2*a*c))/(2*b)
+
+print(f'El resultado es: {resultado}')
+'''
+
+'''
+# Ejercicio 2:
+# Determinar la solución lógica de la siguiente operación
+# ((3+5*8)<3and((-6/3*4)+2<2))or(a>b)
+a = 3
+b = 2
+resultado = ((3+5*8)<3and((-6/3*4)+2<2))or(a>b)
+print(f'El resultado es: {resultado}')
+'''
+
+'''
+# Ejercicio 3:
+# Intercambiar el valor de dos variables.
+# Por ejemplo:
+#    a = 10, b = 5 -> a = 5, b = 10
+a=5
+b=10
+aux=a
+a=b
+b=aux
+print(f'El resultado de a es: {a} y el de b es: {b}')
+'''
+
+# Ejercicio 4:
+# Área y longitud de un círculo
+# Hacer un programa para inresar el radio de un círculo y se reporte su área y la longitud de la circunferencia
+# Área= Pi*2 | Longitud= 2*Pi*r
+# En este ejercicio vamos a necesitar importar el módulo math porque tiene el valor de Pi
+# Se escribe import math
---
+++ [UTN.Laboratorio]-Python/[Python]-Leccion2/main.py
@@ -0,0 +1,28 @@
+#===================================CLASE 6 - MIERCOLES-11/05/2022===================================
+'''condicion = True
+if condicion == True:
+    print('Condicion Verdadera')
+elif condicion == False:
+    print('Condicion Falsa')
+else:
+    print('Condición sin especificar')'''
+
+# num = int(input('Digite un número en el rango del 1 al 3'))
+# numTexto = ''
+# if num == 1:
+#     numTexto = 'Número uno'
+# elif num == 2:
+#     numTexto = 'Número dos'
+# elif num == 3:
+#     numTexto = 'Número tres'
+# else:
+#     numTexto = 'Has ingresado un número fuera de rango'
+# print(f'El número ingresado es: {num} - {numTexto}')
+
+condicion = False
+# if condicion:
+#     print('Condición Verdadera')
+# else:
+#     print('Condición Falsa')
+
+print('Condicion Verdadera') if condicion else print('Condicion Falsa')
