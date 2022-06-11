a1b72bc4a5734c13a9a39ac8da1060cb44497e6f
Aguppesce <aguppesce@gmail.com>
2022-06-11 15:55:53 -0300

    Java Clase 2 13/04/2022

---
+++ [UTN.Programacion]-Java/[Java]-Leccion1/src/HolaMundo.java
@@ -0,0 +1,29 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class HolaMundo {
+        public static void main(String[] args) {
+				//=====================================CLASE 1 - VIERNES-08/04/2022=====================================
+				/*System.out.println("Hola mundo!");*/
+                
+				//=====================================CLASE 2 - MIERCOLES-13/04/2022===================================
+				/*
+                System.out.println("Hola mundo desde Java");
+                int miVariable= 10;
+                System.out.println(miVariable);
+                miVariable=5;
+                System.out.println(miVariable);
+
+                //Tipo String
+                String miVariableCadena = "Bienvenidos";
+                System.out.println(miVariableCadena);
+                miVariableCadena = "Sigamos creciendo en programación";
+                System.out.println(miVariableCadena);
+				*/				
+		
+		}
+}
