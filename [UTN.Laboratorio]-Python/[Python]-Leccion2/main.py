96d79424e77f6ef3d2dc2987efad44cdb9944758
Aguppesce <aguppesce@gmail.com>
2022-06-11 21:59:12 -0300

    Python Clase 6 11/05/2022 Fixed

--- [UTN.Laboratorio]-Python/[Python]-Leccion2/main.py
+++ [UTN.Laboratorio]-Python/[Python]-Leccion2/main.py
@@ -5,24 +5,24 @@ if condicion == True:
 elif condicion == False:
     print('Condicion Falsa')
 else:
-    print('Condición sin especificar')'''
+    print('Condición sin especificar')
 
-# num = int(input('Digite un número en el rango del 1 al 3'))
-# numTexto = ''
-# if num == 1:
-#     numTexto = 'Número uno'
-# elif num == 2:
-#     numTexto = 'Número dos'
-# elif num == 3:
-#     numTexto = 'Número tres'
-# else:
-#     numTexto = 'Has ingresado un número fuera de rango'
-# print(f'El número ingresado es: {num} - {numTexto}')
+ num = int(input('Digite un número en el rango del 1 al 3'))
+ numTexto = ''
+ if num == 1:
+     numTexto = 'Número uno'
+ elif num == 2:
+     numTexto = 'Número dos'
+ elif num == 3:
+     numTexto = 'Número tres'
+ else:
+     numTexto = 'Has ingresado un número fuera de rango'
+ print(f'El número ingresado es: {num} - {numTexto}')
 
 condicion = False
-# if condicion:
-#     print('Condición Verdadera')
-# else:
-#     print('Condición Falsa')
+ if condicion:
+     print('Condición Verdadera')
+ else:
+     print('Condición Falsa')
 
-print('Condicion Verdadera') if condicion else print('Condicion Falsa')
+print('Condicion Verdadera') if condicion else print('Condicion Falsa')'''
