090fbe7028f74ca2c8326453d523ab477b66b608
Aguppesce <aguppesce@gmail.com>
2022-09-23 20:31:41 -0300

    Java II Clase 5 23/09/2022

--- [Branch]-Java_Repo_UTN/[Java]-Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ [Branch]-Java_Repo_UTN/[Java]-Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -11,8 +11,20 @@ public class Aritmetica {
     int b;
 
     //Método
-    public void sumarNumeros(int num1, int num2){
+    public void sumarNumeros() {
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
+    }
+
+    /*============CLASE 05 - VIERNES-23/09/2022============*/
+    public int sumarConRetorno() {
+        return this.a + this.b;
+    }
+
+    public int sumarConArgumentos(int a, int b){
+        this.a = a; //El argumento a se asigna al atributo this.a
+        this.b = b; //
+        //return a + b;
+        return this.sumarConRetorno();
     }
 }
