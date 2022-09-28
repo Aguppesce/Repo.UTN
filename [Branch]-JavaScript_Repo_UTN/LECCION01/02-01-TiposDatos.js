907ba3aebfc544474b532e3a08a095c379cb7b91
Aguppesce <aguppesce@gmail.com>
2022-09-27 22:19:59 -0300

    JavaScript Clase 3 27/09/2022

---
+++ [Branch]-JavaScript_Repo_UTN/LECCION01/02-01-TiposDatos.js
@@ -0,0 +1,20 @@
+//===============CLASE 03 - MARTES-27/09/2022===============
+
+//Tipos de datos en JavaScript
+/**
+ * La sintaxis en lo que es comentarios es muy similar a la de Java realmente diriamos que es identica
+ */
+
+var nombre = "Ariel"; //Tipo String
+console.log(nombre);
+
+var numero = 3000; // Tipo numérico
+console.log(numero);
+
+var objeto = {
+    nombre : "Ariel",
+    apellido : "Betancud",
+    telefono : "26145678933" 
+}
+
+console.log(objeto)
