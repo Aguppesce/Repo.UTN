866653420a9bb7bcb2749ba515afa70c458425a0
Aguppesce <aguppesce@gmail.com>
2022-10-25 22:40:27 -0300

    JavaScript Clase 7 25/10/2022 + Ejercicio Pares/Impares | Ejercicio Mayor de Edad

---
+++ [Branch]-JavaScript_Repo_UTN/LECCION03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,19 @@
+//======================= CLASE 07 - MARTES-25/10/2022=======================
+
+//Ejercicio para encontrar números pares e imapres
+let parImpar = 7;
+if(parImpar % 2 == 0){
+    console.log("Es un número PAR");    
+}else{
+    console.log("Es un número IMPAR")
+}
+
+//Ejercicio: es mayor de edad
+let edad = 20, adulto = 18;
+if(edad >= adulto){
+    console.log("Es una persona adulta");
+} else{
+    console.log("Usted es una persona menor de edad")
+}
+
+
