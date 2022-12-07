7cd5e0417857e4dea4ead839b5546c3cf800e188
Aguppesce <aguppesce@gmail.com>
2022-12-06 22:07:10 -0300

    Python Clase 17 06/12/2022

---
+++ [Branch]-Python_Repo_UTN/[Python]-Leccion14/mundo_pc/dispositivo_entrada.py
@@ -0,0 +1,6 @@
+# ===============CLASE 17 - MARTES-06/12/2022===============
+
+class DispositivoEntrada:
+    def __init__(self, marca, tipo_entrada):
+        self._marca = marca
+        self._tipo_entrada = tipo_entrada
